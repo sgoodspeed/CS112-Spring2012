a0ed3c4ab290bf066019b3f48da676d9c7c41b04
Samuel Goodspeed <srgoodspeed@gmail.com>
2012-01-31 22:46:41 -0500

    hw02 done

---
+++ day2/day2-2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+day2-2.py
+Sam Goodspeed
+"""
+
+
+#print, not printf. Semicolon unnecassary. Pound sign is new comment thing, too
+print "Hello nurse!";
+print "Isn't this a wonderful day?"
+print "Great stuff"
+print "1 + 3=",1+3# , allows you to join multiple things together
+print "2 * 8=",2*8
+print "That was exciting"
